fe8f946b65f561edd16a3a0296a6e94151fe51ce
venurawarnasooriya <venura@ashogu.com>
2023-08-27 22:07:38 +0530

    sidebar-frontend done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { ChatTileComponent } from './Components/chat-tile/chat-tile.component';
+import { ChatComponent } from './Components/chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ChatTileComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
