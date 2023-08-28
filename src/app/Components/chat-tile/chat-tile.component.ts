d4ccea57d248049739c7f5ec750b4c154a8a23e7
venurawarnasooriya <venura@ashogu.com>
2023-08-28 11:07:02 +0530

    header section frontend done

--- src/app/Components/chat-tile/chat-tile.component.ts
+++ src/app/Components/chat-tile/chat-tile.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-chat-tile',
   templateUrl: './chat-tile.component.html',
-  styleUrls: ['./chat-tile.component.css']
+  styleUrls: ['./chat-tile.component.css'],
 })
 export class ChatTileComponent implements OnInit {
+  @Input() chatContent: { role: string; content: string };
+  chatUser: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    console.log(this.chatContent);
+    this.chatUser = this.chatContent.role;
   }
-
 }
