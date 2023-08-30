f1cad71a178148486cb7e2ee291627a3bf885a2f
venurawarnasooriya <venura@ashogu.com>
2023-08-31 00:29:35 +0530

    chat-loading frontend and backend done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './Components/sidebar/sidebar.component';
-import { ChatTileComponent } from './Components/chat-tile/chat-tile.component';
 import { ChatComponent } from './Components/chat/chat.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SidebarComponent,
-    ChatTileComponent,
-    ChatComponent,
-  ],
+  declarations: [AppComponent, SidebarComponent, ChatComponent],
   imports: [HttpClientModule, BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
